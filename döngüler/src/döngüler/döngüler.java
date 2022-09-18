823b120764c09ac6b2f7f0c90eb0c785bf1e1cf3
Can Sarı <bedrancans23@gmail.com>
2022-09-19 02:01:37 +0300

    döngüler

---
+++ döngüler/src/döngüler/döngüler.java
@@ -0,0 +1,30 @@
+package döngüler;
+
+public class döngüler {
+	public static void main(String[] args) {
+		// for döngüsü
+		
+		for(int i=1;i<10;i++) {
+					System.out.println(i);
+				 }
+			System.out.println("for bitti");
+		//while döngüsü
+		int i= 1;
+		while(i<10) {
+			System.out.println(i);
+			i++;
+		}
+		System.out.println("While bitti");
+		//Do-While
+		int j=1;
+		do {
+			System.out.println(j);
+			j= j+1;
+		}while(j<10);
+		System.out.println("Do-While bitti");
+		
+	}
+
+}
+
+
